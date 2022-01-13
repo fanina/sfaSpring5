71efe13232f2deb3d9404528179fd1ab7408104d
safae Fanina <safae.fanina@gmail.com>
2022-01-13 13:59:39 +0100

    Thymeleaf Authors

---
+++ src/main/java/sfa/springframwork/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package sfa.springframwork.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sfa.springframwork.spring5webapp.repositories.AuthorRepository;
+import sfa.springframwork.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
